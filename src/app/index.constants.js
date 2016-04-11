35bb74a33787684df07828fab7cfb6ea83378624
Albert <kovalevskij.albert@gmail.com>
2016-04-11 18:33:18 +0300

    initial commit

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('vseramki')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
---
+++ src/app/index.module.js
@@ -0,0 +1,12 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('vseramki', [
+      'ui.router',
+      'ngMaterial',
+      'toastr',
+      'sistemium'
+    ]);
+
+})();
