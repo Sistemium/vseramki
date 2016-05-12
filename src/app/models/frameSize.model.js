e9756312153377765d3ff2966bf4fd2daf07fa0b
TaWReOn <deniss.mosin@gmail.com>
2016-05-12 22:19:38 +0300

    item frame size model

---
+++ src/app/models/frameSize.model.js
@@ -0,0 +1,32 @@
+'use strict';
+
+(function () {
+
+  angular
+    .module('vseramki')
+    .service('FrameSize', FrameSize)
+    .run(function(FrameSize){
+      FrameSize.findAll();
+    })
+  ;
+
+  function FrameSize(Schema) {
+
+    return Schema.register({
+
+      name: 'FrameSize',
+
+      relations: {
+        hasMany: {
+          Article: {
+            localField: 'articles',
+            foreignKey: 'frameSizeId'
+          }
+        }
+      }
+
+    });
+
+  }
+
+}());
