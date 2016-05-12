351731cc96f860fcaa055ff72adcfe41678e8aec
TaWReOn <deniss.mosin@gmail.com>
2016-05-12 22:19:25 +0300

    item material model

---
+++ src/app/models/material.model.js
@@ -0,0 +1,32 @@
+'use strict';
+
+(function () {
+
+  angular
+    .module('vseramki')
+    .service('Material', Material)
+    .run(function(Material){
+      Material.findAll();
+    })
+  ;
+
+  function Material(Schema) {
+
+    return Schema.register({
+
+      name: 'Material',
+
+      relations: {
+        hasMany: {
+          Article: {
+            localField: 'articles',
+            foreignKey: 'materialId'
+          }
+        }
+      }
+
+    });
+
+  }
+
+}());
