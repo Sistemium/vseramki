f8b01e10e2216c8996473852904aff640dc19fd3
TaWReOn <deniss.mosin@gmail.com>
2016-05-12 22:15:43 +0300

    cart controller

--- src/app/domain/cart/cart.controller.js
+++ src/app/domain/cart/cart.controller.js
@@ -1,26 +1,20 @@
+'use strict';
+
 (function () {
-  'use strict';
 
   angular
     .module('vseramki')
     .controller('CartController', CartController)
   ;
 
-  function CartController(Cart,Article,$scope) {
+  function CartController(Cart, Article, $scope) {
 
     var vm = this;
 
-    Cart.bindAll({},$scope,'vm.data');
-    Cart.findAll().then(function(carts){
-      _.each(carts,function (cart){
+    Cart.bindAll({}, $scope, 'vm.data');
+    Cart.findAll().then(function (carts) {
+      _.each(carts, function (cart) {
         Article.find(cart.articleId);
-        //Cart.loadRelations(cart,['Article'],{adapter: 'http'})
-        //  .then(function(c){
-        //    console.log (c);
-        //  })
-        //  .catch(function(c){
-        //    console.error (c);
-        //  });
       });
     });
 
@@ -29,12 +23,49 @@
     }
 
     function clearItem(item) {
+      item.count = 0;
       Cart.destroy(item);
+    }
+
+    function saveItem(item) {
+      Cart.save(item);
+    }
+
+    function plusOne(item) {
+      item.count++;
+      Cart.save(item);
+    }
+
+    function minusOne(item) {
+
+      item.count--;
+
+      if (item.count < 1) {
+        item.count = 1;
+      }
+
+      Cart.save(item);
+    }
+
+    function itemClick(item){
+      console.log(item);
+    }
+
+
+    /* on remove article add animation */
+
+    function itemRemove(item) {
+      console.log (item, 'item');
     }
 
     angular.extend(vm, {
       clearCart: clearCart,
-      clearItem: clearItem
+      clearItem: clearItem,
+      saveItem: saveItem,
+      plusOne: plusOne,
+      minusOne: minusOne,
+      itemClick: itemClick,
+      itemRemove: itemRemove
     });
 
 
