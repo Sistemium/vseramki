cd54f143485bec0eaf64b049201638797f5a37c4
TaWReOn <deniss.mosin@gmail.com>
2016-05-16 18:58:17 +0300

    filtering

--- src/app/domain/itemView/item.controller.js
+++ src/app/domain/itemView/item.controller.js
@@ -7,7 +7,12 @@
     .controller('ItemController', ItemController)
   ;
 
-  function ItemController($stateParams, Article, Cart) {
+  function ItemController($stateParams, Article, Cart, Schema) {
+
+    var Colour = Schema.model('Colour');
+    var Material = Schema.model('Material');
+    var FrameSize = Schema.model('FrameSize');
+
 
     var vm = this;
 
@@ -31,21 +36,38 @@
     }
 
     function onBlur(article) {
-      console.log(article);
       if (!article.inCart.count) {
         Cart.destroy(article.inCart);
       }
     }
 
     function onCartChange(article) {
-      console.log(article);
       Cart.save(article.inCart);
+      console.log(vm.colour);
     }
 
 
     Article.find($stateParams.id).then(function (article) {
       vm.article = article;
+
+      if (vm.article) {
+
+        Colour.find(vm.article.colourId).then(function (colour) {
+          vm.colour = colour;
+        });
+
+        Material.find(vm.article.materialId).then(function (material) {
+          vm.material = material;
+        });
+
+        FrameSize.find(vm.article.frameSizeId).then(function (size) {
+          vm.size = size;
+        });
+
+      }
+
     });
+
 
     angular.extend(vm, {
       minusOne: minusOne,
@@ -53,7 +75,8 @@
       onBlur: onBlur,
       onCartChange: onCartChange,
       addToCart: Cart.addToCart,
-      price: 33
+      price: 33,
+      article: ''
     });
 
   }
