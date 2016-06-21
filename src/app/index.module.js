387ba51601c761865633b90e96a88a9123af670f
TaWReOn <deniss.mosin@gmail.com>
2016-06-21 17:34:28 +0300

    modules added

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -7,7 +7,12 @@
       'ngMaterial',
       'sistemium',
       'vs-repeat',
-      'md.data.table'
+      'md.data.table',
+      'lfNgMdFileInput',
+      'ngFileUpload',
+      //'jkuri.gallery',  // should be deleted on succeess
+      //'angular-carousel',
+      'jkAngularCarousel'
     ]);
 
 })();
