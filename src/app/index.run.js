3fa39e9c6707f6efaaa0b0ca4b2fc0b4068492a7
TaWReOn <deniss.mosin@gmail.com>
2016-07-15 16:44:27 +0300

    subscription on stateChage added

--- src/app/index.run.js
+++ src/app/index.run.js
@@ -6,9 +6,20 @@
     .run(runBlock);
 
   /** @ngInject */
-  function runBlock($log) {
+  function runBlock($log, $rootScope, $state) {
+
+    var defaultChildHelperSubscription = $rootScope.$on('$stateChangeSuccess', function (event, toState) {
+
+      if (toState.defaultChild) {
+        $state.go('.' + toState.defaultChild);
+      }
+
+    });
+
+    $rootScope.$on('$destroy', defaultChildHelperSubscription);
 
     $log.debug('runBlock end');
+
   }
 
 })();
