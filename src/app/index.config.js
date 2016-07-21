17c47d8b27dfb6ec04951c00de7016c835684995
TaWReOn <deniss.mosin@gmail.com>
2016-07-21 13:40:02 +0300

    text formatting

--- src/app/index.config.js
+++ src/app/index.config.js
@@ -1,19 +1,23 @@
-(function() {
+(function () {
   'use strict';
 
   angular
     .module('vseramki')
     .config(config)
-    .service('DEBUG',DEBUG)
+    .service('DEBUG', DEBUG)
   ;
 
-  function config($logProvider) {
+  function config($logProvider, $mdThemingProvider) {
     // Enable log
     $logProvider.debugEnabled(true);
+
+    // define themes for material
+    $mdThemingProvider.theme('success-toast');
+    $mdThemingProvider.theme('fail-toast');
   }
 
-  function DEBUG (saDebug) {
-    return saDebug.log ('stm:vr');
+  function DEBUG(saDebug) {
+    return saDebug.log('stm:vr');
   }
 
 })();
