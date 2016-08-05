4ce68bd13e73318d4fcddf42a6d936da72b761dc
Alexander Levin <alevin.ru@gmail.com>
2016-08-05 20:36:25 +0300

    propertyFilter.component

---
+++ src/app/components/propertyFilter/propertyFilter.component.js
@@ -0,0 +1,41 @@
+'use strict';
+
+(function () {
+
+  angular.module('vseramki')
+    .component('propertyFilter', {
+
+      templateUrl: 'app/components/propertyFilter/propertyFilter.html',
+
+      bindings: {
+        propertyName: '@',
+        propertyLabel: '@',
+        options: '=',
+        currentOption: '=',
+        setOptionFn: '&',
+        clearFn: '&'
+      },
+
+      controllerAs: 'vm',
+
+      controller: function () {
+
+        var vm = this;
+
+        _.assign(vm, {
+
+          clearFilter: function () {
+            vm.clearFn()(vm.propertyName);
+          },
+
+          optionClick: function (option) {
+            vm.setOptionFn()(option, vm.propertyName);
+          }
+
+        });
+
+      }
+
+    });
+
+})();
