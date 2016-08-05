0525f25a9bcf176910419122e86e343241e19448
TaWReOn <deniss.mosin@gmail.com>
2016-08-05 16:10:19 +0300

    constants added

--- src/app/index.constants.js
+++ src/app/index.constants.js
@@ -1,9 +1,17 @@
+'use strict';
 /* global moment:false */
 (function() {
   'use strict';
 
   angular
     .module('vseramki')
-    .constant('moment', moment);
+    .constant('moment', moment)
+
+    .constant('saaAppConfig', {
+      authUrl: 'https://oauth.it',
+      authApiUrl: 'https://oauth.it/api/',
+      redirect_uri: 'http://localhost:3004',
+      orgAppId: 'c6b1bc54-5a2a-11e6-8000-e188647b398f'
+    })
 
 })();
