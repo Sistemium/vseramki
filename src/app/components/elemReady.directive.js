7a7586ba26ab62a52c74d8042d6f164d09ea859c
TaWReOn <deniss.mosin@gmail.com>
2016-08-12 20:38:38 +0300

    elemReady directive added

---
+++ src/app/components/elemReady.directive.js
@@ -0,0 +1,23 @@
+'use strict';
+
+(function () {
+
+  angular.module('vseramki')
+    .directive('elemReady', function ($parse) {
+      return {
+        restrict: 'A',
+
+        link: function ($scope, elem, attrs) {
+
+          elem.ready(function () {
+            $scope.$apply(function () {
+              var func = $parse(attrs.elemReady);
+              func($scope)(elem);
+            });
+          });
+
+        }
+      }
+    });
+
+})();
