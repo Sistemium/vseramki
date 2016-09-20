29e7b771e110991aeda045195131332804eadb86
Alexander Levin <alevin.ru@gmail.com>
2016-09-20 22:53:13 +0300

    index.constants authUrl config

--- src/app/index.constants.js
+++ src/app/index.constants.js
@@ -3,13 +3,16 @@
 
 (function() {
 
+  var authUrl = 'https://oauth.it';
+  // authUrl = 'http://localhost:9080';
+
   angular
     .module('vseramki')
     .constant('moment', moment)
 
     .constant('saaAppConfig', {
-      authUrl: 'https://oauth.it',
-      authApiUrl: 'https://oauth.it/api/',
+      authUrl: authUrl,
+      authApiUrl: authUrl + '/api/',
       redirect_uri: location.port ? 'http://localhost:3004' : 'https://vr.sistemium.com',
       orgAppId: location.port ? 'c6b1bc54-5a2a-11e6-8000-e188647b398f' : '4cb3ebb4-7f39-11e6-8000-e188647b398f'
     })
