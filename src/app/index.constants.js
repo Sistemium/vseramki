459b7ef6411d233317eab66bba55938f44ad10cb
Alexander Levin <alevin.ru@gmail.com>
2016-09-20 19:53:25 +0300

    production constants

--- src/app/index.constants.js
+++ src/app/index.constants.js
@@ -10,8 +10,8 @@
     .constant('saaAppConfig', {
       authUrl: 'https://oauth.it',
       authApiUrl: 'https://oauth.it/api/',
-      redirect_uri: 'http://localhost:3004',
-      orgAppId: 'c6b1bc54-5a2a-11e6-8000-e188647b398f'
+      redirect_uri: location.port ? 'http://localhost:3004' : 'https://vr.sistemium.com',
+      orgAppId: location.port ? 'c6b1bc54-5a2a-11e6-8000-e188647b398f' : '4cb3ebb4-7f39-11e6-8000-e188647b398f'
     })
 
 })();
