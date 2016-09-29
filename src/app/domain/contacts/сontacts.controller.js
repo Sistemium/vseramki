2ee4ec51ab3c8c3939911a5b8454cd7fe1b76c6f
Alexander Levin <alevin.ru@gmail.com>
2016-09-29 20:24:56 +0300

    contacts with map

--- src/app/domain/contacts/сontacts.controller.js
+++ src/app/domain/contacts/сontacts.controller.js
@@ -7,11 +7,27 @@
     .controller('СontactsController', СontactsController);
 
 
-  function СontactsController() {
+  function СontactsController(MapsHelper) {
 
     var vm = this;
 
-    angular.extend(vm, {});
+    const location = {
+      longitude: 37.7359293,
+      latitude: 55.6530419
+    };
+
+    angular.extend(vm, {
+      map: {
+        zoom: 12,
+        center: MapsHelper.yLatLng(location),
+        marker: MapsHelper.yMarkerConfig({
+          id: 1,
+          location: location,
+          content: 'ООО «Ювит»'
+        })
+      }
+    });
+
   }
 
 }());
