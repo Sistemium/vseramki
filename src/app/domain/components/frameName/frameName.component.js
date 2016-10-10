99a55d81dbd9b0ae42a52b0f05bf21cbd0e065fb
Alexander Levin <alevin.ru@gmail.com>
2016-10-10 16:25:02 +0300

    frameName component

---
+++ src/app/domain/components/frameName/frameName.component.js
@@ -0,0 +1,19 @@
+'use strict';
+
+(function () {
+
+  angular.module('vseramki')
+    .component('frameName', {
+
+      templateUrl: 'app/domain/components/frameName/frameName.html',
+
+      bindings: {
+        article: '='
+      },
+
+      controller: ()=>{},
+      controllerAs: 'vm'
+
+    });
+
+})();
