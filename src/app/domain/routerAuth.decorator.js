c1a28a423594b7a636eaa20f834bceac1ffb4087
Alexander Levin <alevin.ru@gmail.com>
2016-10-10 16:44:05 +0300

    routerAuth decorator

---
+++ src/app/domain/routerAuth.decorator.js
@@ -0,0 +1,56 @@
+(function(){
+
+  function routerAuth($state, AuthHelper, $rootScope) {
+
+    var loggedIn;
+    var loggingIn;
+
+    var trt = $rootScope.$on('$stateChangeStart', function (event, to, toParams) {
+
+      function checkRoles() {
+
+        var needAdmin = _.get(to, 'data.needRoles') || /(edit|add)/g.test(to.name);
+
+        // TODO: check needRoles value
+        if (needAdmin && !AuthHelper.isAdmin()) {
+          event.preventDefault();
+          $state.go('home');
+        }
+      }
+
+      if (loggedIn) {
+        return checkRoles();
+      }
+
+      var user = AuthHelper.hasUser();
+
+      if (!user) {
+        return checkRoles();
+      }
+
+      event.preventDefault();
+
+      // TODO: render a view with a spinner while http checking token
+
+      if (!loggingIn) {
+        loggingIn = user
+          .then(()=> {
+            loggedIn = true;
+            loggingIn = false;
+            if (to.name === 'home') {
+              $state.go('home');
+            } else {
+              $state.go(to, toParams);
+            }
+          });
+      }
+
+    });
+
+    $rootScope.$on('$destroy', trt);
+  }
+
+  angular.module('vseramki')
+    .run(routerAuth);
+
+})();
--- src/app/index.run.js
+++ src/app/index.run.js
@@ -8,10 +8,7 @@
     .run(runBlock);
 
   /** @ngInject */
-  function runBlock($log, $rootScope, $state, AuthHelper, $window) {
-
-    var loggedIn;
-    var loggingIn;
+  function runBlock($log, $window) {
 
     function checkIfDisableScroll (event, element) {
       if (element.nodeName === 'BODY' || event.touches.length > 1) {
@@ -27,51 +24,6 @@
     $window.addEventListener('touchmove', function(event) {
       checkIfDisableScroll(event, event.target);
     }, true);
-
-
-    var trt = $rootScope.$on('$stateChangeStart', function (event, to, toParams) {
-
-      function checkRoles() {
-
-        var needAdmin = _.get(to, 'data.needRoles') || /(edit|add)/g.test(to.name);
-
-        // TODO: check needRoles value
-        if (needAdmin && !AuthHelper.isAdmin()) {
-          event.preventDefault();
-          $state.go('home');
-        }
-      }
-
-      if (loggedIn) {
-        return checkRoles();
-      }
-
-      var user = AuthHelper.hasUser();
-
-      if (!user) {
-        return checkRoles();
-      }
-
-      event.preventDefault();
-
-      // TODO: render a view with a spinner while http checking token
-
-      if (!loggingIn) {
-        loggingIn = user
-          .then(()=> {
-            loggedIn = true;
-            loggingIn = false;
-            if (to.name === 'home') {
-              $state.go('home');
-            } else {
-              $state.go(to, toParams);
-            }
-          });
-      }
-
-    });
-
-    $rootScope.$on('$destroy', trt);
 
     $log.debug('runBlock end');
 
