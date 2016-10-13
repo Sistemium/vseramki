3525fc1deb17abef686a41b8362cb52519622fa3
TaWReOn <deniss.mosin@gmail.com>
2016-10-13 19:36:52 +0300

    entity model

---
+++ src/app/models/entity.model.js
@@ -0,0 +1,39 @@
+'use strict';
+
+(function () {
+
+  angular
+    .module('vseramki')
+    .service('Entity', Entity)
+    .run(function (Entity) {
+      Entity.findAll();
+    })
+  ;
+
+  function Entity(Schema) {
+
+    var model = Schema.register({
+
+      name: 'Entity',
+
+      getDefault: function (entityName) {
+        return _.get(model.get(entityName), 'options.defaultId');
+      },
+
+
+      setDefault: function (item, id) {
+        return model.create({
+          id: item,
+          options: {
+            defaultId: id
+          }
+        });
+      }
+
+    });
+
+    return model;
+
+  }
+
+}());
