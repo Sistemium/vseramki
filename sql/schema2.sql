205e9543b41d994a6cdae1d2c259e1d53b9e15ac
TaWReOn <deniss.mosin@gmail.com>
2016-10-13 19:41:41 +0300

    sql scripts

--- sql/schema2.sql
+++ sql/schema2.sql
@@ -147,3 +147,13 @@ meta.createTable 'BaguetteImage',
 ;
 
 alter table vr.Baguette add unique (brandId,materialId,colourId);
+
+create table [vr2].[Entity] (
+   id ID,
+   [xid] CODE not null,
+   [options] TEXT null,
+   ts TS,
+   cts CTS,
+   primary key(id),
+   unique(xid)
+)
