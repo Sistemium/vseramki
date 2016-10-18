96269d06ff8200530101dbe8555f523441219a2f
TaWReOn <deniss.mosin@gmail.com>
2016-10-18 18:39:08 +0300

    create user if it not existing in DB

--- src/app/domain/userInfo/userInfo.controller.js
+++ src/app/domain/userInfo/userInfo.controller.js
@@ -7,23 +7,45 @@
     .controller('UserInfoController', UserInfoController)
   ;
 
-  function UserInfoController(AuthHelper, Auth) {
+  function UserInfoController(AuthHelper, Auth, Schema, $q) {
 
     var vm = this;
 
-    function setUser() {
-      AuthHelper.hasUser()
-        .then(function (user) {
-          vm.user = user;
-          vm.roles = AuthHelper.userRoles();
-        });
-    }
+    var User = Schema.model('User');
+
+    _.assign(vm, {
+      logout: Auth.logout
+    });
+
+    /*
+    Init
+     */
 
     setUser();
 
-    angular.extend(vm, {
-      logout: Auth.logout
-    });
+    /*
+    Functions
+     */
+
+    function setUser() {
+      var authUser = AuthHelper.getUser();
+      vm.roles = AuthHelper.userRoles();
+
+      User.find(authUser.id)
+        .catch(err => {
+          if (err.status == 404) {
+            return User.create({
+              id: authUser.id,
+              name: authUser.name
+              //TODO: try to get email and phone from saProviderAccount
+            })
+          }
+          return $q.reject(err);
+        })
+        .then(user => vm.user = user)
+        .catch(err => console.error(err));
+
+    }
 
   }
 
