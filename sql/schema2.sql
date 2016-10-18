859de29e0023d019577bfa5ec3def999e615b993
TaWReOn <deniss.mosin@gmail.com>
2016-10-18 17:41:16 +0300

    upgrade-User.sql

--- sql/schema2.sql
+++ sql/schema2.sql
@@ -156,4 +156,13 @@ create table [vr2].[Entity] (
    cts CTS,
    primary key(id),
    unique(xid)
-)
+);
+
+
+meta.defineEntity 'User',
+ 'name;email,name,,nullable;phone,name,,nullable;isDeleted'
+;
+
+meta.createTable 'User',
+  @forceDrop = 1
+;
