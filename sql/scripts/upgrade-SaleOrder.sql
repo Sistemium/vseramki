6715b5e6a060d11b61d7e4a4403148a87e393c54
TaWReOn <deniss.mosin@gmail.com>
2016-10-19 14:37:40 +0300

    SaleOrder script

---
+++ sql/scripts/upgrade-SaleOrder.sql
@@ -0,0 +1,22 @@
+meta.defineType 'date:DATE';
+meta.defineType 'count:INT';
+meta.defineType 'comment:STRING,,nullable';
+
+meta.defineEntity 'SaleOrder',
+ 'shipDate,date,,nullable;comment;' +
+ 'contactName,name;email,name;phone,name;isDeleted',
+ 'User,creatorId,nullable'
+;
+
+meta.defineEntity 'SaleOrderPosition',
+ 'count;price;isDeleted',
+ 'SaleOrder,saleOrderId;Article,articleId'
+;
+
+meta.createTable 'SaleOrder',
+  @forceDrop = 1
+;
+
+meta.createTable 'SaleOrderPosition',
+  @forceDrop = 1
+;
