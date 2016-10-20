0784a23cca325c07d21d8f917f242889a3af062d
TaWReOn <deniss.mosin@gmail.com>
2016-10-20 18:38:58 +0300

    orders page route added

--- src/app/domain/userInfo/userInfo.route.js
+++ src/app/domain/userInfo/userInfo.route.js
@@ -20,6 +20,13 @@
           needRoles: true
         }
       })
+      .state({
+        name: 'order',
+        url: '/order',
+        templateUrl: 'app/domain/userInfo/order.html',
+        controller: 'UserInfoController',
+        controllerAs: 'vm'
+      })
     ;
   }
 
