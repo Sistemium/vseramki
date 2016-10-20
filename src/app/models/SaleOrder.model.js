9285a22231d6fd5114db36b9ee4837d8625cb324
TaWReOn <deniss.mosin@gmail.com>
2016-10-20 12:37:17 +0300

    SaleOrder methods

--- src/app/models/SaleOrder.model.js
+++ src/app/models/SaleOrder.model.js
@@ -9,7 +9,9 @@
   function SaleOrder(Schema) {
 
     return Schema.register({
+
       name: 'SaleOrder',
+
       relations: {
         hasOne: {
           User: {
@@ -19,11 +21,46 @@
         },
         hasMany: {
           SaleOrderPosition: {
-            localField: 'saleOrderPositions',
+            localField: 'positions',
             foreignKey: 'saleOrderId'
           }
         }
+      },
+
+      methods: {
+
+        cost: function () {
+          return Math.round(100.0 * this.count * this.price) / 100.0;
+        },
+
+        orderSubTotal: () => {
+          return _.sumBy(this.positions, item =>
+            item.count > 0 ? Math.round(100.0 * item.priceOrigin * item.count) / 100.0 : 0
+          );
+        },
+
+        orderTotal: function () {
+          var items = this.positions;
+          var subTotal = model.orderSubTotal(items);
+
+          return _.sumBy(items, item => item.count ? item.cost(subTotal) : 0);
+        },
+
+        recalcTotals: function (vm) {
+
+          var model = this;
+          var items = model.positions;
+
+          vm.cartSubTotal = model.orderSubTotal();
+          vm.cartTotal = model.orderTotal();
+          vm.cartItems = items.length;
+          vm.cartHasDiscount = (vm.cartSubTotal > vm.cartTotal);
+          vm.cartDiscount = (1 - vm.cartTotal / vm.cartSubTotal) * 100;
+          vm.cartTotalCount = _.sumBy(items, 'count');
+        }
+
       }
+
     });
 
   }
