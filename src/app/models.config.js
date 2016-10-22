1d773a78da6d658e25f323a3dca46d0cc5e01b0b
Alexander Levin <alevin.ru@gmail.com>
2016-10-23 00:13:59 +0300

    Schema. labelOf

--- src/app/models.config.js
+++ src/app/models.config.js
@@ -6,39 +6,61 @@
     || location.protocol === 'https:' && '/api/'
     || 'https://api.sistemium.com/v4d/vr2/';
 
-  angular
-    .module('vseramki')
+
+  function Schema(saSchema, $http) {
+
+    return saSchema({
+
+      labelOf: function (count) {
+
+        var labels = this.labels.count;
+
+        if (_.inRange(count, 9, 20)) {
+          return labels['567890'];
+        } else {
+          var digit = count % 10;
+          if (digit === 1) {
+            return labels['1'];
+          } else if (_.inRange(count, 2, 4)) {
+            return labels['234'];
+          } else {
+            return labels['567890'];
+          }
+        }
+
+      },
+
+      getCount: function (params) {
+        return $http
+          .get(
+            basePath + this.endpoint,
+            {
+              params: angular.extend({
+                'agg:': 'count'
+              }, params || {})
+            }
+          )
+          .then(function (res) {
+            return parseInt(res.headers('x-aggregate-count'));
+          });
+      }
+    });
+
+  }
+
+  angular.module('vseramki')
+
     .config(function (DSHttpAdapterProvider) {
       angular.extend(DSHttpAdapterProvider.defaults, {
         basePath: basePath
       });
     })
 
-    .factory('Schema', function (saSchema, $http) {
-
-      return saSchema({
-        getCount: function (params) {
-          return $http
-            .get(
-              basePath + this.endpoint,
-              {
-                params: angular.extend({
-                  'agg:': 'count'
-                }, params || {})
-              }
-            )
-            .then(function (res) {
-              return parseInt(res.headers('x-aggregate-count'));
-            });
-        }
-      });
-
-    })
+    .factory('Schema', Schema)
 
     .run(function (DS, $window) {
       var adapter = new $window.DSLocalStorageAdapter();
       DS.registerAdapter('localStorage', adapter);
-    })
-  ;
+    });
 
 }());
