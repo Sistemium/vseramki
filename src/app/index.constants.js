ae720f4d0d208a33c19e25ac82bb399b80597388
Alexander Levin <alevin.ru@gmail.com>
2016-10-28 13:02:11 +0300

    saaAppConfig.loginState

--- src/app/index.constants.js
+++ src/app/index.constants.js
@@ -13,10 +13,11 @@
     .constant('moment', moment)
 
     .constant('saaAppConfig', {
+      loginState: 'login',
       authUrl: authUrl,
       authApiUrl: authUrl + '/api/',
       redirect_uri: localDev ? 'http://localhost:3004' : 'https://vr.sistemium.com',
       orgAppId: localDev ? 'c6b1bc54-5a2a-11e6-8000-e188647b398f' : '4cb3ebb4-7f39-11e6-8000-e188647b398f'
-    })
+    });
 
 })();
