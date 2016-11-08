78eaa03f2a3d94ddea4634b8fea0f3e757a0562d
TaWReOn <deniss.mosin@gmail.com>
2016-11-08 12:41:11 +0200

    export fields

--- src/app/models/meta/ExportConfig.constant.js
+++ src/app/models/meta/ExportConfig.constant.js
@@ -10,22 +10,32 @@
         {
           title: 'Наименование',
           property: 'name'
-        },{
+        }, {
           title: 'Артикул',
           property: 'code'
-        },{
+        }, {
+          title: 'Бренд',
+          property: 'brand.name'
+        }, {
           title: 'Материал',
           property: 'material.name'
-        },{
+        }, {
+          title: 'Цвет',
+          property: 'colour.name'
+        }, {
           title: 'Ширина багета',
           property: 'borderWidth',
           type: 'number'
+        }, {
+          title: 'Поверхность',
+          property: 'surface.name'
+        }, {
+          title: 'Код 1С',
+          property: 'code'
         }
       ],
 
-      Article: [
-
-      ]
+      Article: []
 
     });
 
