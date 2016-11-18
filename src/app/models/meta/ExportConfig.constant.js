76a808768145acb2d4e9efeea9bbfd256342f98a
TaWReOn <deniss.mosin@gmail.com>
2016-11-18 18:38:49 +0200

    Export config Articles

--- src/app/models/meta/ExportConfig.constant.js
+++ src/app/models/meta/ExportConfig.constant.js
@@ -6,6 +6,50 @@
     .module('vseramki')
     .constant('ExportConfig', {
 
+      Article: [
+        {
+          title: 'Код',
+          property: 'code'
+        }, {
+          title: 'Наименование',
+          property: 'name'
+        }, {
+          title: 'Размер',
+          property: 'frameSize.name'
+        }, {
+          title: 'Мин. коробка',
+          property: 'packageRel',
+          type: 'number'
+        }, {
+          title: 'Вес ед., кг',
+          property: 'pieceWeight',
+          type: 'number'
+        }, {
+          title: 'Прозрачная вставка',
+          property: 'screening.name'
+        }, {
+          title: 'Материал',
+          property: 'material.name'
+        }, {
+          title: 'Крепление задника',
+          property: 'backMount.name'
+        }, {
+          title: 'Бренд',
+          property: 'brand.name'
+        }, {
+          title: 'Цвет',
+          property: 'colour.name'
+        }, {
+          title: 'Мел_Опт_Цена',
+          property: 'highPrice',
+          type: 'number'
+        }, {
+          title: 'Спец_Цена',
+          property: 'lowPrice',
+          type: 'number'
+        }
+      ],
+
       Baguette: [
         {
           title: 'Наименование',
@@ -33,9 +77,7 @@
           title: 'Код',
           property: 'codeExternal'
         }
-      ],
-
-      Article: []
+      ]
 
     });
 
