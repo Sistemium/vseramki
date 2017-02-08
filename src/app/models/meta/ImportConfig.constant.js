fc806f81b4ca7edb4318af357967dfbd777d361e
TaWReOn <deniss.mosin@gmail.com>
2017-02-08 12:30:35 +0200

    const to service

--- src/app/models/meta/ImportConfig.constant.js
+++ src/app/models/meta/ImportConfig.constant.js
@@ -2,99 +2,76 @@
 
 (function () {
 
-  var BaguetteColumns = [
-    {
-      name: 'codeExternal',
-      label: 'Код'
-    }, {
-      name: 'nameExternal',
-      label: 'Наименование'
-    }, {
-      name: 'code',
-      label: 'Артикул',
-      replace: false
-    }, {
-      name: 'borderWidth',
-      label: 'Ширина багета',
-      parser: parseInt,
-      defaultValue: 0
-    }, {
-      name: 'colour.name',
-      model: 'Colour',
-      label: 'Цвет',
-      ref: 'colourId'
-    }, {
-      name: 'surface.name',
-      model: 'Surface',
-      label: 'Поверхность',
-      ref: 'surfaceId'
-    }, {
-      name: 'material.name',
-      model: 'Material',
-      label: 'Материал',
-      ref: 'materialId'
-    }, {
-      name: 'brand.name',
-      model: 'Brand',
-      label: 'Бренд',
-      ref: 'brandId',
-      replace: false,
-      compute: item => {
+  function ImportConfigFN(Schema) {
 
-        var name = item['Наименование'];
-        if (!name) {
-          return null;
+    const BaguetteColumns = [
+      {
+        name: 'codeExternal',
+        label: 'Код'
+      }, {
+        name: 'nameExternal',
+        label: 'Наименование'
+      }, {
+        name: 'code',
+        label: 'Артикул',
+        replace: false
+      }, {
+        name: 'borderWidth',
+        label: 'Ширина багета',
+        parser: parseInt,
+        defaultValue: 0
+      }, {
+        name: 'colour.name',
+        model: 'Colour',
+        label: 'Цвет',
+        ref: 'colourId'
+      }, {
+        name: 'surface.name',
+        model: 'Surface',
+        label: 'Поверхность',
+        ref: 'surfaceId'
+      }, {
+        name: 'material.name',
+        model: 'Material',
+        label: 'Материал',
+        ref: 'materialId'
+      }, {
+        name: 'brand.name',
+        model: 'Brand',
+        label: 'Бренд',
+        ref: 'brandId',
+        replace: false,
+        compute: item => {
+
+          var name = item['Наименование'];
+          if (!name) {
+            return null;
+          }
+
+          var re = /"(.+)"/;
+          var res = _.last(name.match(re));
+
+          if (res) {
+            return res;
+          }
+
+          re = / ([А-Я][а-я]+) /;
+          res = _.last(name.match(re));
+
+          return _.upperFirst(res);
+
         }
-
-        var re = /"(.+)"/;
-        var res = _.last(name.match(re));
-
-        if (res) {
-          return res;
-        }
-
-        re = / ([А-Я][а-я]+) /;
-        res = _.last(name.match(re));
-
-        return _.upperFirst(res);
-
       }
-    }
-  ];
+    ];
 
-  var ArticleColumns = [
+    const ArticleColumns = [
 
-    {
-      name: 'codeExternal',
-      label: 'Код'
-    }, {
-      name: 'name',
-      label: 'Наименование'
-    }, {
-      name: 'nameExternal',
-      label: 'Наименование (код 1С)',
-      compute: item => {
-        return item['Наименование']
-      }
-    }, {
-      name: 'frameSize.name',
-      model: 'FrameSize',
-      label: 'Размер',
-      ref: 'frameSizeId',
-      compute: item => {
-
-        var frameSize = item['Размер'];
-        var formattedFrameSize;
-
-        if (!frameSize) {
-          return null;
-        } else {
-          var splitters = ['х', ' ', '*'];
-          _.each(splitters, function (splitter) {
-            var splittedVal = frameSize.split(splitter);
-            if (splittedVal.length == 2) {
-              formattedFrameSize = splittedVal[0] + 'x' + splittedVal[1];
-              return false;
+      {
+        name: 'codeExternal',
+        label: 'Код'
+      }, {
+        name: 'name',
+        label: 'Наименование'
       }, {
         // delete if not needed
         name: 'code',
@@ -103,6 +80,13 @@
           var reg = /^\s+$/;
           var isSpaceString = reg.test(item['Артикул']);
           return isSpaceString ? null : item['Артикул'];
+        }
+      }, {
+        name: 'nameExternal',
+        label: 'Наименование (код 1С)',
+        compute: item => {
+          return item['Наименование']
+        }
       }, {
         name: 'frameSize.name',
         model: 'FrameSize',
@@ -145,51 +129,31 @@
           }
 
         }
+      }, {
+        name: 'packageRel',
+        label: 'Мин. коробка',
+        parser: parseInt
+      }, {
+        name: 'pieceWeight',
+        label: 'Вес ед., кг',
+        parser: parseFloat
+      }, {
+        name: 'screening.name',
+        model: 'Screening',
+        label: 'Прозрачная вставка',
+        ref: 'screeningId'
+      }, {
+        name: 'baguetteId',
+        model: 'Baguette',
+        label: 'Багет',
+        compute: item => {
 
-      }
-    }, {
-      name: 'packageRel',
-      label: 'Мин. коробка',
-      parser: parseInt
-    }, {
-      name: 'pieceWeight',
-      label: 'Вес ед., кг',
-      parser: parseFloat
-    }, {
-      name: 'screening.name',
-      model: 'Screening',
-      label: 'Прозрачная вставка',
-      ref: 'screeningId'
-    }, {
-      name: 'backMount.name',
-      model: 'BackMount',
-      label: 'Крепление задника',
-      ref: 'backMountId'
-    }, {
-      name: 'highPrice',
-      label: 'Розничная цена',
-      //parser: parseFloat,
-      compute: item => {
-        return parseFloat(item['Мел_Опт_Цена']);
-      }
-    }, {
-      name: 'lowPrice',
-      label: 'Оптовая цена',
-      //parser: parseFloat,
-      compute: item => {
-        return parseFloat(item['Спец_Цена']);
-      }
-    }
 
-  ];
           var code = item['Артикул'];
           var regexp = /^X?\d{1,}(?:\*?\d{1,})$/mi;
           var rlkRegexp = /РЛК$/mi;
           var baguetteCode;
 
-  angular
-    .module('vseramki')
-    .constant('ImportConfig', {
           if (code != false) {
 
             var divided = code.split('-');
@@ -259,6 +223,29 @@
           }
 
         }
+      }, {
+        name: 'backMount.name',
+        model: 'BackMount',
+        label: 'Крепление задника',
+        ref: 'backMountId'
+      }, {
+        name: 'highPrice',
+        label: 'Розничная цена',
+        //parser: parseFloat,
+        compute: item => {
+          return parseFloat(item['Мел_Опт_Цена']);
+        }
+      }, {
+        name: 'lowPrice',
+        label: 'Оптовая цена',
+        //parser: parseFloat,
+        compute: item => {
+          return parseFloat(item['Спец_Цена']);
+        }
+      }
+    ];
+
+    return {
 
       Baguette: {
         BaguetteColumns,
@@ -270,6 +257,11 @@
         doneSref: 'catalogue'
       }
 
-    });
+    }
+  }
+
+  angular
+    .module('vseramki')
+    .service('ImportConfig', ImportConfigFN);
 
 })();
