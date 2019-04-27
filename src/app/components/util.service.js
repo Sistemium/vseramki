ded5e18318e88eba1cac958a7d4b367721cc0558
Alexander Levin <alevin.ru@gmail.com>
2019-04-27 11:43:07 +0300

    searchRe etc

--- src/app/components/util.service.js
+++ src/app/components/util.service.js
@@ -3,7 +3,7 @@
   function util() {
     return {
       searchRe(search) {
-        let normalized = _.replace(search, /[ ]+/g, ' ');
+        let normalized = _.replace(_.toLower(search), /[ ]+/g, ' ');
         normalized = _.replace(normalized, /["']/g, '');
         const research = _.map(_.escapeRegExp(normalized), l => {
           switch (l) {
@@ -12,6 +12,9 @@
             case 'е':
             case 'ё':
               return '[её]';
+            case 'и':
+            case 'й':
+              return '[ий]';
             default: return l;
           }
         }).join('');
