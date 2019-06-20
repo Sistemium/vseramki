d2672206b230683063c65d3b3f6acf5324b9e872
Alexander Levin <alevin.ru@gmail.com>
2019-06-20 15:39:18 +0300

    rename pictures

--- src/app/domain/pictures/list/pictureList.component.js
+++ src/app/domain/pictures/list/pictureList.component.js
@@ -14,9 +14,39 @@
 
   });
 
-  function pictureListController($scope, ControllerHelper) {
+  function pictureListController($scope, ControllerHelper, $mdDialog) {
 
-    ControllerHelper.setup(this, $scope);
+    ControllerHelper.setup(this, $scope)
+      .use({
+        renameClick(event, picture) {
+          event.stopPropagation();
+          showEditDialog(picture, event);
+        },
+      });
+
+
+    function showEditDialog(picture, targetEvent) {
+
+      const {renamed, name} = picture;
+
+      const confirm = $mdDialog.prompt()
+        .title('Переименовать файл?')
+        .textContent(renamed)
+        .placeholder(name)
+        // .ariaLabel('Dog name')
+        .initialValue(renamed || name)
+        .targetEvent(targetEvent)
+        // .required(true)
+        .ok('Переименовать')
+        .cancel('Отмена');
+
+      $mdDialog.show(confirm)
+        .then(result => {
+          picture.renamed = result === name ? null : result;
+          return picture.DSCreate();
+        }, _.noop);
+
+    }
 
   }
 
