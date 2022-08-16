65b3a43624284d5fbe4c1fa4b6b1db4607c0491f
Alexander Levin <alevin.ru@gmail.com>
2022-08-16 17:32:06 +0300

    Update index.constants.js

--- src/app/index.constants.js
+++ src/app/index.constants.js
@@ -17,7 +17,7 @@
       loginState: 'login',
       authUrl: authUrl,
       authApiUrl: authUrl + '/api/',
-      redirect_uri: localDev ? 'http://localhost:3004' : 'https://xn--80adjnjl0ag.xn--p1ai',
+      redirect_uri: localDev ? 'http://localhost:3004' : 'https://vr.sistemium.ru',
       orgAppId: localDev ? 'c6b1bc54-5a2a-11e6-8000-e188647b398f' : '4cb3ebb4-7f39-11e6-8000-e188647b398f'
     });
 
